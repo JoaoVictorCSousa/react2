1630d71b92122663ab3556f2604ab3ec9bdadcdb
Nome <email@host.com>
2022-11-23 12:13:09 -0300

    initial commit

---
+++ projetoreact2/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import {Home} from './components/Home/Home'
+
+function App() {
+ return(
+  <Home />
+ )
+  ;
+}
+
+export default App;
---
+++ projetoreact2/src/components/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+        
+            <h1 className='titulo'> Home</h1>
+            <img src='https://i.imgur.com/V8XOfJk.jpeg' alt='imagem tela inicial' className='img'/>
+        
+
+        </>
+
+    )
+}
+
+export {Home};
